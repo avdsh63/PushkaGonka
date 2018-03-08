bbc8143a364d4c1192cd2950077d876cf2717b89
avdsh63 <avdsh.evgeniy@gmail.com>
2018-03-08 14:03:39 +0400

    a

--- Test1.js
+++ Test1.js
@@ -91,6 +91,11 @@ const test_case_1 = async () => {
 
 }
 
+const test_case_2 = async () => {
+    const start = Date.now()
+    await driver.sleep (1000)
+    await login_by_avdsh()
+}
 
 
 driver.get('https://curator.develop.wowworks.ru/auth/login');
